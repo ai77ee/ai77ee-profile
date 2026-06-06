4befa3538638b2392f5546f4124e6e3010e4caf4
ai77ee <12903667+ai77ee@users.noreply.github.com>
2026-06-06 09:22:30 -0600

    first versio of the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,45 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [
+    {
+      name: 'Jersey 25',
+      cssVariable: '--font-jersey-25',
+      provider: fontProviders.fontsource(),
+      // Specify weights that are actually used
+      weights: [400],
+      // Specify styles that are actually used
+      styles: ['normal'],
+      // Download more font formats
+      formats: ['woff2', 'woff'],
+    },
+    {
+      name: 'Share Tech Mono',
+      cssVariable: '--font-share-tech-mono',
+      provider: fontProviders.fontsource(),
+      // Specify weights that are actually used
+      weights: [400],
+      // Specify styles that are actually used
+      styles: ['normal'],
+      // Download more font formats
+      formats: ['woff2', 'woff'],
+    },
+    {
+      name: 'Tranquility',
+      cssVariable: '--font-tranquility',
+      provider: fontProviders.local(),
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/tranquility.ttf'],
+          },
+        ],
+      },
+    },
+  ],
+  adapter: vercel(),
+});
